f11ea7f64035dfa4c55068f442708279b8eea335
dad <dad@localhost.localdomain>
2018-03-01 20:37:27 +0000

    Revise yaml, general cleanup

--- snap/scripts/uglify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SNAPDIR=$(pwd)
-SNAPTMP=$(mktemp -d)
-cd ${SNAPTMP}
-
-uglifyjs -o data/js/bootstrap.min.js data/js/bootstrap.js
-uglifyjs -o data/js/ekko-lightbox.min.js data/js/ekko-lightbox.js
